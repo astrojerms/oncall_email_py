85ae2a95c7521c2be4929bc17732abcf18cd7dbf
Astrojerms <jerms13@gmail.com>
2020-02-24 16:26:51 -0800

    added prompts for username and password

--- oncall_email.py
+++ oncall_email.py
@@ -7,14 +7,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.options import Options
 
+import getpass
+
 options = Options()
 options.headless = True
+
+user = input("Username: ") 
+pw = getpass.getpass("Password: ")
+
+
 driver = webdriver.Firefox()
 driver.get("https://vplanner.dst.ibm.com/team/index/60665/")
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "dijitDialogCloseIcon")))
 driver.find_element_by_class_name("dijitDialogCloseIcon").click()
 inputUsername = driver.find_element_by_name("email")
-inputUsername.send_keys("EMAIL_ADDR")
-driver.find_element_by_name("password").send_keys("PASSWORD")
+inputUsername.send_keys(user)
+driver.find_element_by_name("password").send_keys(pw)
 driver.find_element_by_name("submit").click()
 driver.find_element_by_class_name("dijitDialogCloseIcon").click()
